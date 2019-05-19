3db30546c6a912cdff29623fd19fd57bf772c04e
kkvvnn <kkvvnn89@gmail.com>
2019-05-19 11:16:51 +0500

    end of first lesson

--- public/index.php
+++ public/index.php
@@ -1,9 +1,19 @@
 <?php
 
-header('Content-Type: text/html; charset=utf-8');
-
-$query = $_SERVER['QUERY_STRING'];
+$query = rtrim($_SERVER['QUERY_STRING'], '/');
+echo $query;
 
 require '../vendor/core/Router.php';
+require '../vendor/libs/functions.php';
 
-$router = new Router();
+Router::add('posts/add', ['controller' => 'Post', 'action' => 'add']);
+Router::add('posts', ['controller' => 'Post', 'action' => 'index']);
+Router::add('', ['controller' => 'Main', 'action' => 'index']);
+
+debug(Router::getRoutes());
+
+if (Router::matchRoute($query)) {
+    debug(Router::getRoute());
+} else {
+    echo 'Привет 404';
+}
