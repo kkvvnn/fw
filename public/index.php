ec4633680f640212f0b733a927a9eae467d60abb
kkvvnn <kkvvnn89@gmail.com>
2019-05-19 14:57:34 +0500

    add default route

--- public/index.php
+++ public/index.php
@@ -6,9 +6,11 @@ echo $query;
 require '../vendor/core/Router.php';
 require '../vendor/libs/functions.php';
 
-Router::add('posts/add', ['controller' => 'Post', 'action' => 'add']);
-Router::add('posts', ['controller' => 'Post', 'action' => 'index']);
-Router::add('', ['controller' => 'Main', 'action' => 'index']);
+// Router::add('posts/add', ['controller' => 'Post', 'action' => 'add']);
+// Router::add('posts', ['controller' => 'Post', 'action' => 'index']);
+// Router::add('', ['controller' => 'Main', 'action' => 'index']);
+
+Router::add('^$', ['controller' => 'Main', 'action' => 'index']);
 
 debug(Router::getRoutes());
 
